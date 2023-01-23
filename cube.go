e07101d582966c27d08c7177227c19e53927a9bb
Juliano Soares da Silva <julianosoaresdasilva@outlook.com>
2023-01-23 20:23:16 -0300

    add ReplaceProvider and Run

--- cube.go
+++ cube.go
@@ -6,6 +6,8 @@ type Test interface {
 
 type Cube interface {
 	AddProvider(provider any) error
+	ReplaceProvider(provider any) error
+	Run(runner any) (error, error)
 	AddRunner(runner any) error
 	AddTest(test any)
 }
