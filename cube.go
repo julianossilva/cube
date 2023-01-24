b05a8bdb4e6ebcad4d2c18fc4f8995f5fe1cd5b2
Juliano Soares da Silva <julianosoaresdasilva@outlook.com>
2023-01-23 21:07:12 -0300

    add Container

--- cube.go
+++ cube.go
@@ -6,8 +6,11 @@ type Test interface {
 
 type Cube interface {
 	AddProvider(provider any) error
-	ReplaceProvider(provider any) error
-	Run(runner any) (error, error)
 	AddRunner(runner any) error
 	AddTest(test any)
+}
+
+type Container interface {
+	ReplaceProvider(provider any) error
+	Run(runner any) (error, error)
 }
